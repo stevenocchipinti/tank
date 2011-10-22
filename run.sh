e422879663f78a8a6effd6fc4de58b89cdaaf278
Steven Occhipinti <dev@stevenocchipinti.com>
2011-10-22 18:57:03 +1100

    Wrapper script to start the system

--- run.sh
+++ run.sh
@@ -10,13 +10,19 @@
 # of the other, serve it up and repeat infinitely.
 #
 
+# TODO: These processes should be managed by the Foreman gem!
+
 # This example will stream the webcam over HTTP
 #mjpg_streamer -i "input_uvc.so" -o "output_http.so -w /usr/www/ -p 8080" &
 
-# Stream each webcam on a separate port
+# Stream the files from each webcam on a separate port
 mjpg_streamer -i "input_file.so -f img/cam0 -r" -o "output_http.so -w www -p 8080" &
 mjpg_streamer -i "input_file.so -f img/cam1 -r" -o "output_http.so -w www -p 8081" &
 
+# Start Sinatra!
+bundle exec ruby www/control.rb &
+
+# Create the files from alternating snapshots from the webcams
 x=0
 while :
 do
